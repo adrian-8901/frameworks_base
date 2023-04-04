93fda132b73d143bee83a1c64615f00b8e78d1fc
adrian-8901 <adrianlam230@gmail.com>
2023-04-04 23:15:06 +0800

    [DNM] themes: Initalize systemSettings

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -391,6 +391,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mUserTracker = userTracker;
         mResources = resources;
         mWakefulnessLifecycle = wakefulnessLifecycle;
+        mSystemSettings = systemSettings;
         dumpManager.registerDumpable(TAG, this);
     }
 
